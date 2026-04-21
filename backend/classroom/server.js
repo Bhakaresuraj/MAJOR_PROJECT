0e832492ffb737a412143f0af2e3fec4ab330c66
Bhakaresuraj <surajbhakare190@gmail.com>
2026-04-21 09:48:52 +0530

    Learning Sessions in express

--- backend/classroom/server.js
+++ backend/classroom/server.js
@@ -1,25 +1,48 @@
 const express = require("express");
 const app = express();
-const users = require("./routes/user");
-const posts = require("./routes/post");
-const cookies = require("./routes/cookies");
-const cookieParser = require("cookie-parser");
-app.use(cookieParser("secretcode"));
-app.use("/users", users);
-app.use("/posts", posts);
-app.use("/cookies", cookies);
 
 
+const session = require('express-session')
 
-// Cookies and signed cookies 
+app.use(session({
+    secret :"mysupersecreatestring"
+}))
 
-app.get("/", (req, res) => {
-    let { name = "Dipak" } = req.cookies;
-    let { greet } = req.signedCookies;
-    console.log(greet);
-    res.send(`Hey ${name} , ${greet} !`);
-
+app.get("/test",(req,res)=>{
+    res.send("test successful !"); 
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const users = require("./routes/user");
+// const posts = require("./routes/post");
+// const cookies = require("./routes/cookies");
+// const cookieParser = require("cookie-parser");
+// app.use(cookieParser("secretcode"));
+
+// app.use("/users", users);
+// app.use("/posts", posts);
+// app.use("/cookies", cookies);
+
+// // Cookies and signed cookies 
+
+// app.get("/", (req, res) => {
+//     let { name = "Dipak" } = req.cookies;
+//     let { greet } = req.signedCookies;
+//     console.log(greet);
+//     res.send(`Hey ${name} , ${greet} !`);
+
+// })
 
 app.listen(3000, () => {
     console.log("Server is running ....");
