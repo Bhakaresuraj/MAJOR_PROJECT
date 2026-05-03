4866bf4cb8456c1f055b3f863df48ac31de7f251
Bhakaresuraj <surajbhakare190@gmail.com>
2026-05-03 10:51:24 +0530

    modified for adding authentication in project

--- backend/app.js
+++ backend/app.js
@@ -110,10 +110,7 @@ app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong......" } = err;
     res.status(statusCode).render("error.ejs", { err });
     // res.status(statusCode).send(message);
-})
-
-
-
+});
 
 //  listing the request.........
 let port = 8080;
