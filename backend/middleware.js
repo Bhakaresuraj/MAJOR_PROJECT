1482e64f9332449ae75400ad7c87357e9317bea6
Bhakaresuraj <surajbhakare190@gmail.com>
2026-05-03 13:38:18 +0530

    Learned how to map login signup routes , req.originalUrl ,req.path

--- backend/middleware.js
+++ backend/middleware.js
@@ -1,7 +1,15 @@
 module.exports.isLogedIn = (req, res, next) => {
+    console.log(req.path);
+    console.log(req.originalUrl);
     if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error", "To Add listing you need to Ligin ...!");
         return res.redirect("/login");
     }
+    next();
+}
+module.exports.redirectUrl = (req, res, next) => {
+    res.locals.redirectUrl = req.session.redirectUrl;
+    console.log("middleware :", res.locals.redirectUrl); 
     next();
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,9 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-
-
-
+const { redirectUrl } = require("../middleware.js");
 // signup route......
 router.get("/signup", (req, res) => {
     res.render("Users/signup.ejs");
@@ -15,9 +13,15 @@ router.post("/signup", wrapAsync(async (req, res) => {
         let { username, email, password } = req.body;
         const newUser = new User({ email, username });
         let registereduser = await User.register(newUser, password);
-        console.log("registereduser", registereduser);
-        req.flash("success", "User Registered Successfully...!")
-        res.redirect("/listing")
+        // console.log("registereduser", registereduser);
+        req.login(registereduser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Wandarlust...!")
+            res.redirect("/listing")
+        })
+
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -28,15 +32,20 @@ router.post("/signup", wrapAsync(async (req, res) => {
 router.get("/login", (req, res) => {
     res.render("Users/login.ejs");
 })
-router.post("/login", passport.authenticate('local',
+router.post("/login", redirectUrl, passport.authenticate('local',
     {
         failureRedirect: '/login',
         failureFlash: true
     }
-), async (req, res) => {
-    req.flash("success", "You are loged in successfully....!");
-    res.redirect("/listing");
-});
+)
+    , async (req, res) => {
+
+
+        req.flash("success", "You are loged in successfully....!");
+        let redirectUrl = res.locals.redirectUrl || "/listing";
+        console.log("route :  ",redirectUrl);
+        res.redirect(redirectUrl);
+    });
 
 
 
